28b1ad20c9e7351dcec4932f055fe24a9839cc6c
Simon Richard <simonrichard@users.noreply.github.com>
2017-07-20 14:58:53 -0400

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="csv-bsplit",
-    version="0.1.0",
+    version="0.2.0",
     py_modules=["csv_bsplit"],
     install_requires=["humanfriendly"],
     entry_points={
